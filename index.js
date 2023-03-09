861a448a55c9353f70684eff620d961c9548891d
ang <57077187+constSobri@users.noreply.github.com>
2023-03-09 01:03:29 -0600

    si

---
+++ index.js
@@ -0,0 +1,13 @@
+let nombre = 'Angel'
+let apellido = 'Pacheco'
+
+const obj = {
+    "nombre": nombre,
+    "apellido": apellido
+}
+
+//localStorage.setItem("Angel", JSON.stringify(obj))
+//sessionStorage.setItem("Angel", JSON.stringify(obj))
+
+//document.cookie = "nombre =" + JSON.stringify(obj) + "expires=" + new Date(2023,5,1)
+
